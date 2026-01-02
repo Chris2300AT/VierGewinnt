c1cbfaaf6fafec5c593ffa372fc29e73ae8dcce3
chris <christopherbraun1950@outlook.de>
2026-01-02 01:21:58 +0100

    Comments and minimal changes

--- src/main/java/at/ac/hcw/viergewinnt/CenterController.java
+++ src/main/java/at/ac/hcw/viergewinnt/CenterController.java
@@ -3,16 +3,20 @@ package at.ac.hcw.viergewinnt;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+//Controller of the center part of the BorderPane
 public class CenterController {
 
     @FXML
     private Label infoLabel;
 
+    //Selected Category can be used for Filesystem
     private String selectedCategory;
 
     // Called from MainController whenever a new category is selected
     public void initData(String selectedCategory) {
         this.selectedCategory = selectedCategory;
+
+        //example output of the Category
         infoLabel.setText("Currently selected: " + selectedCategory);
     }
 }
